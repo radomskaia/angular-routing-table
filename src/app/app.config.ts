31975919533895c811de597d8ce4323c06819fe4
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-06-25 21:11:06 +0700

    fix: add HTTP client provider to app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
   ],
 };
