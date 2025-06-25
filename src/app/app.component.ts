10dfa55cbc411333f9bd652f7882562d5ff7cd1c
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-06-25 20:41:55 +0700

    feat: implement header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   selector: 'app-root',
   styleUrl: './app.component.scss',
   templateUrl: './app.component.html',
---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-header',
+  styleUrl: './header.component.scss',
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {}
